04900cc5fa330ce6e7c0fb24c9112fb52576a4c9
makyurt <44249574+makyurt@users.noreply.github.com>
2020-06-23 03:31:28 +0300

    updated node count script

--- docs/nodeinfo.php
+++ docs/nodeinfo.php
@@ -9,6 +9,8 @@ $context = stream_context_create(
 	
 $data=json_decode(file_get_contents('https://api.github.com/repos/digibyte/digibyte/releases', false, $context),true);
 
+$data2=json_decode(file_get_contents('https://api.github.com/repos/DigiByte-Core/digibyte/releases', false, $context),true);
+
 $sum = 0;
 
 foreach ($data[0]['assets'] as $value) {
@@ -48,6 +50,10 @@ $sum += $value['download_count'];
 }
 
 foreach ($data[9]['assets'] as $value) {
+$sum += $value['download_count'];
+}
+
+foreach ($data2[0]['assets'] as $value) {
 $sum += $value['download_count'];
 }
 
